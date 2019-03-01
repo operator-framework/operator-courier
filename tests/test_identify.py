7cef58f0dedb32ec1359702084def912c85dcc47
Wenlin Huang <wenlhuan@redhat.com>
2019-03-01 12:15:10 -0500

    Handle malformed yaml files
    - This PR fixes issue https://github.com/operator-framework/operator-courier/issues/16
    - Log with error when a malformed YAML is being parsed
    - Log with info when valid YAML has been parsed
    - Suppress error traceback when calling the CLI

--- tests/test_identify.py
+++ tests/test_identify.py
@@ -1,11 +1,13 @@
 from unittest import TestCase
 import pytest
 import operatorcourier.identify as identify
+from testfixtures import LogCapture
+
 
 @pytest.mark.parametrize('fname,expected', [
-("tests/test_files/csv.yaml", "ClusterServiceVersion"),
-("tests/test_files/crd.yaml", "CustomResourceDefinition"),
-("tests/test_files/package.yaml", "Package"),
+    ("tests/test_files/csv.yaml", "ClusterServiceVersion"),
+    ("tests/test_files/crd.yaml", "CustomResourceDefinition"),
+    ("tests/test_files/package.yaml", "Package"),
 ])
 def test_get_operator_artifact_type(fname, expected):
     with open(fname) as f:
@@ -14,11 +16,34 @@ def test_get_operator_artifact_type(fname, expected):
 
 
 @pytest.mark.parametrize('fname', [
-("tests/test_files/invalid.yaml"),
-("tests/test_files/empty.yaml"),
+    "tests/test_files/invalid.yaml",
+    "tests/test_files/empty.yaml",
 ])
-@pytest.mark.xfail(raises=ValueError)
 def test_get_operator_artifact_type_assertions(fname):
     with open(fname) as f:
         yaml = f.read()
+
+    with pytest.raises(ValueError) as e, LogCapture() as logs:
         identify.get_operator_artifact_type(yaml)
+
+    logs.check(
+        ('operatorcourier.identify', 'ERROR', 'Courier requires valid CSV, CRD, and Package files'),
+    )
+    assert 'Courier requires valid CSV, CRD, and Package files' == str(e.value)
+
+
+@pytest.mark.parametrize('fname', [
+    "tests/test_files/invalid.malformed.parser.error.yaml",
+    "tests/test_files/invalid.malformed.scanner.error.yaml",
+])
+def test_get_operator_artifact_type_with_invalid_yaml(fname):
+    with open(fname) as f:
+        yaml = f.read()
+
+    with pytest.raises(ValueError) as e, LogCapture() as logs:
+        identify.get_operator_artifact_type(yaml)
+
+    logs.check(
+        ('operatorcourier.identify', 'ERROR', 'Courier requires valid input YAML files'),
+    )
+    assert 'Courier requires valid input YAML files' == str(e.value)
