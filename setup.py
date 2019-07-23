0826dc42ee4b345cf5aaeda1f0e107408940f95a
kevinrizza <krizza@redhat.com>
2019-07-23 10:15:59 -0400

    Update version to 2.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
 setup(
   name='operator-courier',
   packages=['operatorcourier'],
-  version='2.1.2',
+  version='2.1.3',
   description='Build, verify and push operators',
   author='AOS Marketplace',
   author_email='aos-marketplace@redhat.com',
