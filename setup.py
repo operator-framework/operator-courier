6069b25083f6cef5c5a4305001de5f18367e0729
kevinrizza <krizza@redhat.com>
2019-08-08 14:30:42 -0400

    Increment version to v2.1.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
 setup(
   name='operator-courier',
   packages=['operatorcourier'],
-  version='2.1.3',
+  version='2.1.4',
   description='Build, verify and push operators',
   author='AOS Marketplace',
   author_email='aos-marketplace@redhat.com',
