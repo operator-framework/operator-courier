125c4508fe2052647a1f84f5c09ca7cae2b18d11
Lance <lgallett@redhat.com>
2019-08-16 13:34:11 -0400

    Incrementing version to 2.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
 setup(
   name='operator-courier',
   packages=['operatorcourier'],
-  version='2.1.6',
+  version='2.1.7',
   description='Build, verify and push operators',
   author='AOS Marketplace',
   author_email='aos-marketplace@redhat.com',
